27b61d40f952db29dba1b1ccd5f70be7b462ae09
Juan Carlos García Murillo <garciamjuancarlos14@gmail.com>
2021-12-04 20:37:32 -0600

    Adición de los modelos Estado, Municipio y Localidad al admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Estado, Municipio, Localidad
+
 # Register your models here.
+admin.site.register(Estado)
+admin.site.register(Municipio)
+admin.site.register(Localidad)
