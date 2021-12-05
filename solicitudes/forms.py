59c9fe30ce6c959ce443a13ebc32145b5ec20757
GabrielCuriel1999 <38192397@uaz.edu.mx>
2021-12-05 02:07:46 -0600

    Se modificó el modelo, formulario y las vistas de la app de solicitudes

--- solicitudes/forms.py
+++ solicitudes/forms.py
@@ -9,13 +9,13 @@ class DateInput(forms.DateInput):
 class SolicitudForm(forms.ModelForm):
     fecha = forms.DateField(label='Fecha',
                             widget=DateInput(
-                                format='%Y-%m-%d',
+                                format='%dd-%mm-%Y',
                                 attrs={'class': 'form-control'}),
                             required=False)
 
     fecha_nacimiento = forms.DateField(label='Fecha de nacimiento',
                                        widget=DateInput(
-                                           format='%Y-%m-%d',
+                                           format='%dd-%mm-%Y',
                                            attrs={'class': 'form-control'}),
                                        required=False)
     
@@ -39,9 +39,9 @@ class SolicitudForm(forms.ModelForm):
             'curp': forms.TextInput(attrs={'class': 'form-control'}),
             'correo': forms.TextInput(attrs={'class': 'form-control'}),
             'resumen': forms.Textarea(attrs={'class': 'form-control'}),
-            'estado':forms.Select(attrs={'class':'form-select'}),
-            'municipio':forms.Select(attrs={'class':'form-select'}),
-            'localidad':forms.Select(attrs={'class':'form-select'}),
-            'estatus':forms.Select(attrs={'class':'form-select'}),
+            'estado':forms.Select(attrs={'class':'form-control', 'placeholder':'Estado'}),
+            'municipio':forms.Select(attrs={'class':'form-control', 'placeholder':'Municipio'}),
+            'localidad':forms.Select(attrs={'class':'form-control', 'placeholder':'Localidad'}),
+            'estatus':forms.Select(attrs={'class':'form-control', 'placeholder':'Estatus'}),
             'usuario':forms.HiddenInput(attrs={'class':'form-control'})
         }
--- solicitudes/views.py
+++ solicitudes/views.py
@@ -1,18 +1,34 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Solicitud
+from usuarios.models import Usuario
 from .forms import SolicitudForm
 
 
-class ListaSolicitud(ListView):
+class ListaSolicitud(LoginRequiredMixin,ListView):
     model = Solicitud
     context_object_name = 'solicitudes'
     
+    def get(self,request,*args,**kwargs):
+        user = request.user
+        if user.is_superuser == 1:
+            return super().get(request,*args,**kwargs)
+        # Se obtiene el usuario logueado
+        usuario = get_object_or_404(Usuario,username=request.user.username)
+        # Es capturista, se muestran solamente las solicitudes que el usuario ha registrado
+        lista_solicitudes = Solicitud.objects.filter(usuario=usuario.id)
+        # Se obtiene asigna el context_object_name definido 
+        self.object_list = self.get_queryset()
+        # Se agregan las solicitudes filtradas a la lista de variables a pasar al template
+        context = self.get_context_data()
+        context['solicitudes'] = lista_solicitudes
+        return self.render_to_response(context)
     
 class NuevaSolicitud(LoginRequiredMixin, CreateView):
     model = Solicitud
@@ -25,6 +41,10 @@ class NuevaSolicitud(LoginRequiredMixin, CreateView):
         return super(NuevaSolicitud, self).form_invalid(form)
     
     def form_valid(self, form):
+        self.solicitud = form.save(commit=False)
+        usuario = get_object_or_404(Usuario,username=self.request.user.username)
+        self.solicitud.usuario = usuario
+        self.solicitud.save()
         messages.success(self.request, 'Solicitud guardada exitosamente.')
         return super(NuevaSolicitud, self).form_valid(form)
     
