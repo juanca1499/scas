3570b5a41c1d2cc5f2dcb068d0449ac69dfc96f8
Juan Carlos García Murillo <garciamjuancarlos14@gmail.com>
2021-12-05 23:30:21 -0600

    Prueba de aceptación de listar usuarios

---
+++ pruebas_aceptacion/features/steps/listar_usuarios.py
@@ -0,0 +1,6 @@
+from behave import given, when, then
+import time
+
+@then(u'puedo ver una lista con todos los usuarios registrados en el sistema.')
+def step_impl(context):
+    context.test.assertIn('Lista de usuarios', context.driver.page_source)
